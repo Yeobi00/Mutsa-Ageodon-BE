e308f6a9d0b2b54ba4df5db219147e0f5a2583a8
yeobi <kimhyjjang@gmail.com>
2024-07-18 17:18:13 +0900

    add like

--- like/models.py
+++ like/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from auths.models import MutsaUser
-from posts.models import Post
+from postt.models import Post
 
 class Like(models.Model):
     LID = models.AutoField(primary_key=True)
---
+++ like/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers,status
+from rest_framework.exceptions import APIException
+
+from .models import Like
+
+class LikeSerializer(serializers.model_meta):
+    class Meta:
+        model = Like
+        fields = ['LID', 'like_date', 'PID', 'UID']
+
+        def create(self, validated_data):
+            like = Like.objects.create(**validated_data)
+            return like
+        
